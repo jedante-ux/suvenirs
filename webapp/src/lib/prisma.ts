860b8e5516384f8330e1464a414bb3b2b53ba328
Jose Edward Ochoa <jedante@MacBook-Pro-de-Jose-2.local>
2026-03-16 08:07:44 -0400

    fix: revert prisma.ts to simple form, rely on NODE_TLS_REJECT_UNAUTHORIZED=0

--- webapp/src/lib/prisma.ts
+++ webapp/src/lib/prisma.ts
@@ -1,13 +1,8 @@
 import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from 'pg'
 
 function createPrismaClient() {
-  const pool = new Pool({
-    connectionString: process.env.DATABASE_URL!,
-    ssl: { rejectUnauthorized: false },
-  })
-  const adapter = new PrismaPg(pool)
+  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
   return new PrismaClient({ adapter })
 }
 
